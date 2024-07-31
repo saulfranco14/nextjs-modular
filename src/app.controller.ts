dc371b4ded3b53a21401ce815935868251ddd635
Saul Franco <sfranco@tiprotec.com>
2024-07-31 16:12:13 -0600

    the first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,9 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return 'Hola mundo!';
-  }
+  getHello(): string { return 'Hola mundo!';}
 
   @Get('nuevo')
   newEndpoint() {
