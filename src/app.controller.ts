c75b8f9832864965e8811a09efc2b5dbd02e57f7
Saul Franco <sfranco@tiprotec.com>
2024-07-31 16:15:37 -0600

    resolved conflicts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string { return 'Hola mundo!';}
+  getHello(): string {
+    return 'Hola mundo!';
+  }
 
   @Get('nuevo')
   newEndpoint() {
